b3a9fbcf9d4c1f4cb6929e60811351b5d8f4cc89
Ahmed Nagy <99285520+ahmed5353@users.noreply.github.com>
2024-02-08 19:28:34 +0200

    Add files via upload

--- src/contexts/QuizContext.js
+++ src/contexts/QuizContext.js
@@ -80,7 +80,7 @@ function QuizProvider({ children }) {
   );
 
   useEffect(function () {
-    fetch(`http://localhost:8000/questions`)
+    fetch(`https://my-json-server.typicode.com/ahmed5353/Quiz-App/questions/`)
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataRecieved", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
